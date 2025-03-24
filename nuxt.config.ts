e67880175dacca5a5b579e886ff489ca2cc988b4
Jonathan Aldas <jonaaldas@gmail.com>
2025-03-24 22:39:13 +0100

    removed-clerk

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt', '@clerk/nuxt', '@nuxtjs/color-mode'],
+  modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt'],
   shadcn: {
     prefix: '',
     componentDir: './components/ui',
